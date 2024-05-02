fae38f61fd4ae1a04c722325ffbfed20a757e952
ShubhKakkar <webkoala1998@gmail.com>
2024-05-02 09:12:43 +0530

    Support page completed

--- src/components/modals/DeleteAccountModal.js
+++ src/components/modals/DeleteAccountModal.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Modal } from "react-responsive-modal";
+import 'react-responsive-modal/styles.css';
 
 const DeleteAccountModal = ({ isOpen, onClose, onDelete }) => {
   return (
